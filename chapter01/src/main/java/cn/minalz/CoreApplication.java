b6477d3b35cf6e7e480c13826eed79ba8aa3d983
minalz <119687281@qq.com>
2021-12-09 14:25:02 +0800

    multi-module first commit

---
+++ chapter01/src/main/java/cn/minalz/CoreApplication.java
@@ -0,0 +1,17 @@
+package cn.minalz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * @author minalz
+ */
+@SpringBootApplication
+public class CoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreApplication.class, args);
+    }
+
+}
---
+++ common/src/main/java/cn/minalz/pojo/UserInfo.java
@@ -0,0 +1,5 @@
+package cn.minalz.pojo;
+
+public class UserInfo {
+
+}
